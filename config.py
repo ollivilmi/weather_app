684b59e7b75651730f9096c2f5c3d400aaa5bb01
ollivilmi <olli.vilmi@gmail.com>
2018-01-11 17:07:12 +0200

    config fix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig(object):
 	DEBUG = False
-	SECRET_KEY = ''\x10@\x8b\xf5\xfeQw\x89\xca\xe8\x0fb\x8d\xe78\x1e\x83\x8696\x04\xf1@''
+	SECRET_KEY = '\x10@\x8b\xf5\xfeQw\x89\xca\xe8\x0fb\x8d\xe78\x1e\x83\x8696\x04\xf1@'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SQLALCHEMY_ECHO = True
